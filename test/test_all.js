fb1f8230a6a63565d011ecda540d12f2281c9c6d
Brendan Duncan <brendanduncan@gmail.com>
2023-04-10 10:38:59 -0700

    enable all tests

--- test/test_all.js
+++ test/test_all.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 import * as test from "./test.js";
 
-//import * as scanner from "./tests/test_scanner.js";
-//import * as parser from "./tests/test_parser.js";
-//import * as struct from "./tests/struct.js";
+import * as scanner from "./tests/test_scanner.js";
+import * as parser from "./tests/test_parser.js";
+import * as struct from "./tests/struct.js";
 import * as reflect from "./tests/test_reflect.js";
-//import * as struct_layout from "./tests/struct_layout.js";
+import * as struct_layout from "./tests/struct_layout.js";
 
 function displayResults() {
   document.body.appendChild(document.createElement("p"));
