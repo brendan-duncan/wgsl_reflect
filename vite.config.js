539071247ba2be4976d67a67be2ffa7f754e91df
feng <908087098@qq.com>
2023-07-26 17:30:52 +0800

    初始化构建方式

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         lib: {
             // Could also be a dictionary or array of multiple entry points
             entry: resolve(__dirname, 'src/index.ts'),
-            name: 'webgpu',
+            name: 'wgsl_reflect',
             // the proper extensions will be added
             fileName: 'index'
         },
