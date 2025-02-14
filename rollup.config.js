5e6d486afc6ff2ddae884f07b417d2d3b480c6ad
brendan-duncan <brendanduncan@gmail.com>
2025-02-14 09:30:14 -0700

    put wgsl_debugger into its own build target

--- rollup.config.js
+++ rollup.config.js
@@ -11,6 +11,9 @@ function build(input, format, file, sourcemap) {
 let builds = [
     build('src/index.ts', 'esm', 'wgsl_reflect.module.js', true),
     build('src/index.ts', 'cjs', 'wgsl_reflect.node.js', true),
+
+    build('src/debugger_index.ts', 'esm', 'wgsl_debugger.module.js', true),
+    build('src/debugger_index.ts', 'cjs', 'wgsl_debugger.node.js', true),
 ];
 
 export default builds;
