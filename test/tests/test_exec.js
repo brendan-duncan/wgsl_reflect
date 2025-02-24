e081dd201ef9da6ef7b682a9e8379ec7fd8cd258
Brendan Duncan <brendanduncan@gmail.com>
2025-02-23 23:44:40 -0700

    add workgroupBarrier test

--- test/tests/test_exec.js
+++ test/tests/test_exec.js
@@ -699,6 +699,81 @@ export async function run() {
             test.closeTo(imageBuffer, webgpuData);
         });
 
+        await test("workgroupBarrier", async function (test) {
+            const shader = `const chunkWidth = 256;
+                const chunkHeight = 1;
+                const chunkSize = chunkWidth * chunkHeight;
+                var<workgroup> bins: array<atomic<u32>, chunkSize>;
+                @group(0) @binding(0) var<storage, read_write> chunks: array<array<u32, chunkSize>>;
+                @group(0) @binding(1) var ourTexture: texture_2d<f32>;
+
+                const kSRGBLuminanceFactors = vec3f(0.2126, 0.7152, 0.0722);
+                fn srgbLuminance(color: vec3f) -> f32 {
+                    return saturate(dot(color, kSRGBLuminanceFactors));
+                }
+
+                @compute @workgroup_size(chunkWidth, chunkHeight, 1)
+                fn cs(
+                    @builtin(global_invocation_id) global_invocation_id: vec3u,
+                    @builtin(workgroup_id) workgroup_id: vec3u,
+                    @builtin(local_invocation_id) local_invocation_id: vec3u,
+                ) {
+                    let size = textureDimensions(ourTexture, 0);
+                    let position = global_invocation_id.xy;
+                    if (all(position < size)) {
+                        let numBins = f32(chunkSize);
+                        let lastBinIndex = u32(numBins - 1);
+                        let color = textureLoad(ourTexture, position, 0);
+                        let v = srgbLuminance(color.rgb);
+                        let bin = min(u32(v * numBins), lastBinIndex);
+                        atomicAdd(&bins[bin], 1u);
+                    }
+
+                    workgroupBarrier();
+
+                    let chunksAcross = (size.x + chunkWidth - 1) / chunkWidth;
+                    let chunk = workgroup_id.y * chunksAcross + workgroup_id.x;
+                    let bin = local_invocation_id.y * chunkWidth + local_invocation_id.x;
+
+                    chunks[chunk][bin] = atomicLoad(&bins[bin]);
+                }`;
+
+            const chunks = new Uint32Array(256 * 256);
+
+            const size = [16, 16];
+            const inTexture = new Uint8Array(size[0] * size[1] * 4);
+            for (let y = 0, idx = 0; y < size[1]; ++y) {
+                for (let x = 0; x < size[0]; ++x, idx += 4) {
+                    inTexture[idx + 0] = x * (255 / size[0]);
+                    inTexture[idx + 1] = y * (255 / size[1]);
+                    inTexture[idx + 2] = 0;
+                    inTexture[idx + 3] = 255;
+                }
+            }
+
+            const descriptor = {
+                dimension: "2d",
+                size: size,
+                mipLevelCount: 1,
+                sampleCount: 1,
+                format: "rgba8unorm",
+                usage: GPUTextureUsage.COPY_DST | GPUTextureUsage.TEXTURE_BINDING
+            };
+
+            const bg = {0: {
+                0: chunks,
+                1: {texture: inTexture, descriptor},
+            }};
+
+            const _data = await webgpuDispatch(shader, "cs", 1, bg);
+            const webgpuData = new Uint32Array(_data);
+
+            const wgsl = _newWgslExec(shader);
+            wgsl.dispatchWorkgroups("cs", 1, bg);
+
+            test.equals(chunks, webgpuData);
+        });
+
         await test("textureLoad", async function (test) {
             const shader = `
                 @group(0) @binding(0) var<storage, read_write> bins: array<u32>;
